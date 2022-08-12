66094633ea23de37fbb1031b02f94e54a9faeb73
Jina Wilde <jina.wilde@my.utsa.edu>
2022-08-12 15:37:21 -0500

    created MVVM TreeView directories

---
+++ WpfTreeView/WpfTreeView/Class1.cs
@@ -0,0 +1,20 @@
+﻿/// this class is unnecessary, just for demo
+
+using PropertyChanged;
+using System.ComponentModel;
+
+namespace WpfTreeView
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Class1 : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged = (sender, e) => { };
+
+        public string Test { get; set; }
+
+ 
+
+       
+    }
+}
